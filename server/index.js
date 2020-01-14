237f8b124dd031a07bc8be127d9ddd4fc90a762b
TomNPython <tom.nicholson1000@gmail.com>
2020-01-14 11:01:20 +0700

    home component added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./queries')
+const cors = require('cors');
+const db = require('./queries');
 
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
