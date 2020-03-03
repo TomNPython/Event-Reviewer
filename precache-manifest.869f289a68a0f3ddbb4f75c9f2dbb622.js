1be2af0a9a61ed70694910222b66148d456f5200
TomNPython <tom.nicholson1000@gmail.com>
2020-03-03 10:27:57 +0700

    Updates

---
+++ precache-manifest.869f289a68a0f3ddbb4f75c9f2dbb622.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fd4ffe9f8fc2ded1581f59779b9c48",
+    "url": "/Event-Reviewer/index.html"
+  },
+  {
+    "revision": "1761b5df494edebfd146",
+    "url": "/Event-Reviewer/static/css/2.08c74b9a.chunk.css"
+  },
+  {
+    "revision": "e4d386a5a454f05491bb",
+    "url": "/Event-Reviewer/static/css/main.c6f89f43.chunk.css"
+  },
+  {
+    "revision": "1761b5df494edebfd146",
+    "url": "/Event-Reviewer/static/js/2.3360388b.chunk.js"
+  },
+  {
+    "revision": "0d51a26e357591260d29df3c0b1559d1",
+    "url": "/Event-Reviewer/static/js/2.3360388b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4d386a5a454f05491bb",
+    "url": "/Event-Reviewer/static/js/main.631b0860.chunk.js"
+  },
+  {
+    "revision": "1a5c25a093ff4eb617d7",
+    "url": "/Event-Reviewer/static/js/runtime-main.84fb1f8a.js"
+  },
+  {
+    "revision": "1d09b670268aca767a720dc5134ff1f4",
+    "url": "/Event-Reviewer/static/media/writing-bg.1d09b670.jpg"
+  }
+]);
